d536e7bbb02b9a0ce354dbd7b99e568e34a090de
Austin Swinney <austin@citrusbyte.com>
2016-08-15 14:41:01 -0400

    comment. lines.

--- bin/ec2_list.rb
+++ bin/ec2_list.rb
@@ -29,35 +29,25 @@ options.verbose = false
 options.tag = "Owner"
 
 opt_parser = OptionParser.new do |opts|
-
   opts.banner = "Usage: ec2_list.rb [options]"
 
-
   opts.on("-r", "--region REGION", "which region to look in ( default: us-west-2 )") do |r|
-
     options[:region] = r
-
   end
 
   # will dump out the aws response in case you're trying to find something to add.
   opts.on("-d", "--debug", "Debug ( spits out AWS dump )") do |d|
-
     options[:debug] = d
-
   end
 
   # will basically dump out the all the tags or whatever is added to verbose later.
   opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-
     options[:verbose] = v
-
   end
 
   # you can look for other tags...
   opts.on("-t", "--tag TAG", "Looking for specific tag ( default: Owner )") do |t|
-
     options[:tag] = t
-
   end
 
 end
@@ -79,9 +69,7 @@ resp = ec2_client.describe_instances
 
 # dump instance if you're looking for a needle in a haystack
 if options[:debug]
-
   pp resp
-
 end
 
 #############################
@@ -91,47 +79,35 @@ out = nil
 
 # you might not get anything back
 if resp.reservations.any?
-
   # for each response record
   resp.each do | inst |
-
     # grab the instance info
-    inst.reservations.each do | reserv |
 
+    inst.reservations.each do | reserv |
       # as far as i can tell there is only one instance per
       instance = reserv.instances[0]
-
       puts "============================"
       puts "Instance ID:\t#{instance.instance_id}"
       puts "Instance Type\t#{instance.instance_type}"
       puts "Launch Time:\t#{instance.launch_time}"
 
       specific_tag = nil
-
-      # looking for existance of the Owner tag
+      # looking for existance of a tag
       instance.tags.each do | t |
 
         if t.key == options.tag
-
           specific_tag=t.value
-
         elsif options[:verbose]
-
           # puts all tags
           puts "Tag:\t#{t.key}=#{t.value}"
-
         end
 
       end
 
       if specific_tag
-
         puts "Tag:\t#{options.tag}=#{specific_tag}"
-
       else
-
         puts "Tag:\t#{options.tag}=unknown"
-
       end
 
     end
@@ -139,7 +115,5 @@ if resp.reservations.any?
   end
 
 else
-
   puts "Nothing found in #{options.region}."
-
 end
